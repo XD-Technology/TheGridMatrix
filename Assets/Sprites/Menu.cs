c094581bb7c73f9a2c29c0a0f857283891072c06
ddaytonxavier <derrickdayton1234@gmail.com>
2021-04-02 07:25:20 +0530

    Menu completed

---
+++ Assets/Sprites/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheGridMatrix
+{
+    public class Menu : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
